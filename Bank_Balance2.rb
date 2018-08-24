19cff9a1aae9b1c6e7994c9298498ec54299f9de
Matthew Martin <m.martin747@gmail.com>
2018-08-24 13:44:15 +1000

    new version 2 runs using arrays and will close program after 3 password attempts

--- Bank_Balance2.rb
+++ Bank_Balance2.rb
@@ -1,5 +1,3 @@
-
-
 puts "welcome to the banking app"
 #password function
 puts "what is you password?"
@@ -7,31 +5,38 @@ input = gets.chomp
 password= "password"
 application_runtime= true
 
-until input == password
+count=0
+until input == password or count== 2
     puts "input invalid, try again"
+    count +=1
     input = gets.chomp
 end
+if input != password 
+    puts "restart app-incorrect password"
+    return 
+end
 system "clear"
+
+
+
 #main menu    
 balance = 0
 
 while application_runtime
 
-    puts "Welcome what would you like to do? (options:Deposit; Balance; Quit)"
-    b1 = "balance"
-    b2 = "deposit"
-    b3 = "quit"
-    b4 = "withdraw"
+    puts "Welcome what would you like to do? (options:Deposit; Withdraw Balance; Quit)"
+    b = ["random","balance","deposit","quit","withdraw"]
+       
     options = gets.chomp
-    until options == b1 or b2 or b3
+    until options == b[1] or b[2] or b[3]
         puts "input invalid"
         options = gets.chomp
     end
 
     # balance function
-    if options == b1
+    if options == b[1]
         puts "Your balance is #{balance}"
-        elsif options == b2
+    elsif options == b[2]
         # desposit function
         puts "your balance is #{balance}"
         puts "how much would you like to deposit"
@@ -42,11 +47,11 @@ while application_runtime
         end
         balance = balance + input_deposit
         puts "your balance is #{balance}"   
-    elsif options == b3
+    elsif options == b[3]
         #quit functions
         application_runtime=false
         puts "good bye"
-    elsif options == b4
+    elsif options == b[4]
         #withdraw function
         puts "your balance is #{balance}"
         puts "how much would you like to withdraw"
@@ -57,7 +62,6 @@ while application_runtime
         end
         balance = balance - input_withdrawal
         puts "your balance is #{balance}"
-        system "clear"
     end
 end
 
