c6d1f7e3d66c45dcfb418edfce7d83bb9afe1054
Matthew Martin <m.martin747@gmail.com>
2018-08-24 10:16:25 +1000

    Desposit and withdraw function added accepting 0 as a value

--- Bank_Balance1.rb
+++ Bank_Balance1.rb
@@ -21,6 +21,7 @@ while application_runtime
     b1 = "balance"
     b2 = "deposit"
     b3 = "quit"
+    b4 = "withdraw"
     options = gets.chomp
     until options == b1 or b2 or b3
         puts "input invalid"
@@ -35,19 +36,27 @@ while application_runtime
         puts "your balance is #{balance}"
         puts "how much would you like to deposit"
         input_deposit = gets.chomp.to_f
-        while input_deposit <1
+        while input_deposit <= -1
             puts "no deposit made"
             input_deposit = gets.chomp.to_f
         end
         balance = balance + input_deposit
-        puts "you're balance is #{balance}"   
+        puts "your balance is #{balance}"   
     elsif options == b3
         #quit functions
         application_runtime=false
         puts "good bye"
+    elsif options == b4
+        #withdraw function
+        puts "your balance is #{balance}"
+        puts "how much would you like to withdraw"
+        input_withdrawal = gets.chomp.to_f 
+        while input_withdrawal >= balance 
+            puts "insufficient balance for withdrawal"
+            input_withdrawal = gets.chomp.to_f 
+        end
+        balance = balance - input_withdrawal
+        puts "your balance is #{balance}"
     end
-
 end
-
-
 
